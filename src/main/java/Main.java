50e7eeb3ad02693e3846e9c7cf2e159ab0416641
Olga Ermolaeva <photo-deti@mail.ru>
2023-06-13 15:32:43 +0700

    Решение лаб1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,60 @@ public class Main {
     public static void main(String[] args){
 
         Random random = new Random();
-        random.nextInt(2000);
-        //int i = new Random().nextInt(2000);
+        int i = new Random().nextInt(2000);
         System.out.println(i);
+        int n = bit(i);
+        System.out.println(n);
+        int[] m1 = getMultiples(n,i);
+        int[] m2 = getNonMultiples(n,i);
 
+
+    }
+
+    static int bit(int x){
+        int t = 0;
+        while(x > 1) {
+            x >>= 1;
+            t++;
+        }
+        return t;
+    }
+
+    static int[] getMultiples(int n, int i){
+        int size = getArraySize(n, i);
+        int[] m1 = new int[size];
+        int k = 0;
+        for (int j = i; j < Short.MAX_VALUE; j++){
+            if (j%n == 0) {
+                m1[k] = j;
+                k++;
+            }
+        }
+        return m1;
+    }
+
+    static int[] getNonMultiples(int n, int i){
+        int size = 0;
+        for (int j = Short.MIN_VALUE; j < i; j++){
+            if (j%n != 0) size++;
+        }
+        int[] m2 = new int[size];
+        int k = 0;
+        for (int j = Short.MIN_VALUE; j < i; j++){
+            if (j%n != 0) {
+                m2[k] = j;
+                k++;
+            }
+        }
+        return m2;
+    }
+
+    static int getArraySize(int n, int i){
+        int size = 0;
+        for (int j = i; j < Short.MAX_VALUE; j++){
+            if (j%n == 0) size++;
+        }
+        return size;
     }
 
 }
