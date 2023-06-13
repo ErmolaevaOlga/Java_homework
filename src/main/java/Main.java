d8501a09d5105739eaf99b0c44e75c21aa3d09f0
Olga Ermolaeva <photo-deti@mail.ru>
2023-06-13 14:19:33 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+//1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
+//2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+//3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+//4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
+
+
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args){
+
+        Random random = new Random();
+        random.nextInt(2000);
+        //int i = new Random().nextInt(2000);
+        System.out.println(i);
+
+    }
+
+}
